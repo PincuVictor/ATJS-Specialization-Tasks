fc03dae9197871f6ebcf8e3e1138635e59884481
PincuVictor <andreipnk2004@gmail.com>
2026-03-01 22:38:46 +0200

    Module12 Cucumber TAF + Full CI integration

--- Module5CY/cypress.config.js
+++ Module5CY/cypress.config.js
@@ -2,11 +2,11 @@ const { defineConfig } = require('cypress');
 
 module.exports = defineConfig({
     reporter: 'cypress-mochawesome-reporter',
+    userAgent:
+        'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/120.0.0.0 Safari/537.36',
+    chromeWebSecurity: false,
     e2e: {
         baseUrl: 'https://practicesoftwaretesting.com',
-        userAgent:
-            'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/120.0.0.0 Safari/537.36',
-        chromeWebSecurity: false,
         viewportWidth: 1280,
         viewportHeight: 720,
         defaultCommandTimeout: 10000,
