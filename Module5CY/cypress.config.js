29f8cd14ee06d90a956e13d82e436b20c741cc8b
PincuVictor <andreipnk2004@gmail.com>
2026-03-01 22:48:33 +0200

    Module12 Cucumber TAF + Full CI integration

--- Module5CY/cypress.config.js
+++ Module5CY/cypress.config.js
@@ -1,20 +1,21 @@
 const { defineConfig } = require('cypress');
 
 module.exports = defineConfig({
-    chromeWebSecurity: false,
     userAgent:
         'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/120.0.0.0 Safari/537.36',
+    chromeWebSecurity: false,
 
     e2e: {
+        baseUrl: 'https://practicesoftwaretesting.com',
+
         viewportWidth: 1280,
         viewportHeight: 720,
         defaultCommandTimeout: 10000,
+        requestTimeout: 10000,
 
         setupNodeEvents(on, config) {
-            require('cypress-mochawesome-reporter/plugin')(on);
             on('before:browser:launch', (browser = {}, launchOptions) => {
                 if (browser.family === 'chromium' && browser.name !== 'electron') {
-                    // Force the user agent into the browser launch arguments
                     launchOptions.args.push(
                         '--user-agent=Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/120.0.0.0 Safari/537.36'
                     );
